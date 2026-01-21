900a1abccdf3a138d759425baaeccbab6373ea61
FedorVX <valayev2005@gmail.com>
2026-01-21 09:25:16 +0300

    crud-app

---
+++ crud-app/src/main/java/com/example/demo/service/ItemService.java
@@ -0,0 +1,44 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Item;
+import com.example.demo.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ItemService {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    public List<Item> getAllItems() {
+        return itemRepository.findAll();
+    }
+
+    public Optional<Item> getItemById(Long id) {
+        return itemRepository.findById(id);
+    }
+
+    public Item createItem(Item item) {
+        return itemRepository.save(item);
+    }
+
+    public Optional<Item> updateItem(Long id, Item itemDetails) {
+        return itemRepository.findById(id)
+                .map(item -> {
+                    item.setName(itemDetails.getName());
+                    return itemRepository.save(item);
+                });
+    }
+
+    public boolean deleteItem(Long id) {
+        return itemRepository.findById(id)
+                .map(item -> {
+                    itemRepository.delete(item);
+                    return true;
+                }).orElse(false);
+    }
+}
